463dfd66f955494dd20b688a86f8921d29f704ac
LundyBernard <lundy.bernard@gmail.com>
2022-05-03 17:08:32 -0500

    add weather endpoints to server

--- bat/server/server.py
+++ bat/server/server.py
@@ -1,7 +1,12 @@
 from flask import Flask
 import connexion
 
-from bat.lib import hello_world
+from bat.lib import (
+    get_temperature,
+    get_presure,
+    get_wind,
+    hello_world,
+)
 
 app = Flask(__name__)
 
@@ -9,6 +14,21 @@ app = Flask(__name__)
 @app.route('/')
 def hello_api():
     return hello_world()
+
+
+@app.route('/temperature')
+def temperature_api():
+    return get_temperature()
+
+
+@app.route('/presure')
+def presure_api():
+    return get_presure()
+
+
+@app.route('/wind')
+def wind_api():
+    return get_wind()
 
 
 def start_server(host='0.0.0.0', port='5000', debug=True):
