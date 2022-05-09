89943171a5191dae0eb625269e086ee9bf8a93c6
LundyBernard <lundy.bernard@gmail.com>
2022-05-09 12:32:58 -0500

    GREEN: functional tests working

--- bat/tests/common_api_tests.py
+++ bat/tests/common_api_tests.py
@@ -10,30 +10,42 @@ class CommonAPITest(object):
     and as local service.
     '''
 
+    def setUp(t):
+        t.location = 'AABP'
+
     def test_api_hello_world(t):
         print('test_api_hello_world')
-        url = f'{t.service_address}hello_world'
+        url = f'{t.service_address}'
         print(f'connect to service at {url}')
         out = requests.get(url, verify=False)
-        t.assertEqual(out.text, '"Hello World!"\n')
+        t.assertEqual(out.text, 'Hello World!')
 
     def test_api_temperature(t):
         print('test_api_temperature')
-        url = f'{t.service_address}temperature'
+        url = f'{t.service_address}temperature/{t.location}'
         print(f'connect to service at {url}')
         out = requests.get(url, verify=False)
-        t.assertEqual(out.text, '"+ VALID OUTPUT +"\n')
+        t.assertEqual(
+            out.text,
+            '75 F (24 C)',  # these values may need to be regex matches
+        )
 
     def test_api_presure(t):
         print('test_api_presure')
-        url = f'{t.service_address}presure'
+        url = f'{t.service_address}presure/{t.location}'
         print(f'connect to service at {url}')
         out = requests.get(url, verify=False)
-        t.assertEqual(out.text, '"+ VALID OUTPUT +"\n')
+        t.assertEqual(
+            out.text,
+            '29.77 in. Hg (1008 hPa)',
+        )
 
     def test_api_wind(t):
         print('test_api_wind')
-        url = f'{t.service_address}wind'
+        url = f'{t.service_address}wind/{t.location}'
         print(f'connect to service at {url}')
         out = requests.get(url, verify=False)
-        t.assertEqual(out.text, '"+ VALID OUTPUT +"\n')
+        t.assertEqual(
+            out.text,
+            'from the W (270 degrees) at 10 MPH (9 KT):0',
+        )
--- functional_tests/service_test.py
+++ functional_tests/service_test.py
@@ -1,12 +1,14 @@
 from unittest import TestCase
 
 PT_SVC_ADDR = 'http://0.0.0.0:5000/'
-#PT_SVC_ADDR = 'http://127.0.0.1/project'
+#PT_SVC_ADDR = 'http://127.0.0.1:5000/'
 
 from bat.tests.common_api_tests import CommonAPITest
 
 
 class ServiceFunctionalTest(TestCase, CommonAPITest):
 
-    def setUp(self):
-        self.service_address = PT_SVC_ADDR
+    def setUp(t):
+        CommonAPITest.setUp(t)
+        t.service_address = PT_SVC_ADDR
+        #t.location = 'AABP'
